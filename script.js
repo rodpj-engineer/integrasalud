d061b245d2b5283378311a85724f087fc857f8ee
Rodrigo PJ <69771178+rodpj-engineer@users.noreply.github.com>
2023-12-12 07:31:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 function calculate() {
     var inputNumber = document.getElementById('numberInput').value;
     var resultElement = document.getElementById('result');
+    var resultInfoElement = document.getElementById('resultInfo');
 
-    // Example table for demonstration
-    var table = {
-        161527556: 'Rodrigo Pereira',
-        140903795: 'Jose Gutierrez',
-        3: 'Answer C'
-        // Add more entries as needed
-    };
+    // Fetch the CSV file
+    fetch('BBDDBupaIntegraSalud.csv')  // CSV File with ruts bupas
+        .then(response => response.text())
+        .then(data => {
+            // Parse the CSV data
+            var rows = data.split('\n');
+            var table = {};
 
-    // Check if inputNumber exists in the table
-    if (table[inputNumber]) {
-        resultElement.innerText = 'Hola ' + table[inputNumber] + ', tenemos las siguientes sugerencias para ti: \n \n XXXX \n \n Agenda ahora aquí! \n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
-    } else {
-        resultElement.innerText = 'Hola, ' + inputNumber + ' lamentablemente no te conocemos, agenda una consulta para poder cuidarte mejor: \n \n AGENDA AQUÍ:\n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
-    }
+            // Populate the table from CSV
+            rows.forEach(row => {
+                var columns = row.split(',');
+                var entry = {
+                    name: columns[1],
+                    age: columns[2],
+                    sex: columns[3],
+                    lastVisit: columns[4],
+                    group: columns[5],
+                    days: columns[6]
+                };
+                table[columns[0]] = entry;
+            });
+
+            // Check if inputNumber exists in the table
+            if (table[inputNumber]) {
+                var resultEntry = table[inputNumber];
+                resultElement.innerText = 'Result: ' + resultEntry.name + ' (' + resultEntry.age + ' years old, ' + resultEntry.sex + ')';
+                resultInfoElement.innerText = 'Last Visit: ' + resultEntry.lastVisit + ', Group: ' + resultEntry.group + ', Days: ' + resultEntry.days;
+
+                //resultElement.innerText = 'Hola ' + table[inputNumber] + ', tenemos las siguientes sugerencias para ti: \n \n XXXX \n \n Agenda ahora aquí! \n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
+                
+            } else {
+                resultElement.innerText = 'Hola, ' + inputNumber + ' lamentablemente no te conocemos, agenda una consulta para poder cuidarte mejor: \n \n AGENDA AQUÍ:\n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
+                resultInfoElement.innerText = '';
+            }
+        })
+        .catch(error => console.error('Error fetching CSV file:', error));
+}
+
+function clearInput() {
+    document.getElementById('numberInput').value = '';
+    document.getElementById('result').innerText = '';
+    document.getElementById('resultInfo').innerText = '';
 }
