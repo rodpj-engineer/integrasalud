7028f0f39b9c950cf72e96db8872cbf50edde13d
Rodrigo PJ <69771178+rodpj-engineer@users.noreply.github.com>
2023-12-11 23:06:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function calculate() {
 
     // Check if inputNumber exists in the table
     if (table[inputNumber]) {
-        resultElement.innerText = 'Hola ' + table[inputNumber] + ', tenemos las siguientes sugerencias para ti: \n \n XXXX \n \n Agenda ahora aquí! ';
+        resultElement.innerText = 'Hola ' + table[inputNumber] + ', tenemos las siguientes sugerencias para ti: \n \n XXXX \n \n Agenda ahora aquí! \n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
     } else {
-        resultElement.innerText = 'Hola, ' + inputNumber + ' lamentablemente no te conocemos, agenda una consulta para poder cuidarte mejor: \n \n AGENDA AQUÍ';
+        resultElement.innerText = 'Hola, ' + inputNumber + ' lamentablemente no te conocemos, agenda una consulta para poder cuidarte mejor: \n \n AGENDA AQUÍ:\n https://www.integramedica.cl/integramedica/tu-salud-al-dia-2';
     }
 }
